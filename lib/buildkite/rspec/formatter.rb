73347f2e34bb0c2e4f910af01b5e0d5f70c5253a
Jack Chen (chendo) <github@chen.do>
2016-12-01 01:04:41 +1100

    Format updates

--- lib/buildkite/rspec/formatter.rb
+++ lib/buildkite/rspec/formatter.rb
@@ -11,16 +11,22 @@ module Buildkite
       end
 
       def example_group_started(notification)
-        output.puts "--- #{'––' * @group_level} #{notification.group.description}" if @group_level < @max_depth
+        output.puts "--- #{prefix} #{notification.group.description}" if @group_level < @max_depth
         super
       end
 
       def example_started(notification)
-        output.puts "--- #{'––' * @group_level} #{notification.example.description}" if ENV['BUILDKITE_RSPEC_BREAK_ON_EXAMPLE']
+        output.puts "--- #{prefix} #{notification.example.description}" if ENV['BUILDKITE_RSPEC_BREAK_ON_EXAMPLE']
+      end
+
+      def prefix
+        return "" if @group_level == 0
+        "#{'  ' * [@group_level - 1, 0].max}⊢–"
       end
 
       def example_failed(notification)
         output.puts "+++ #{'––' * @group_level} #{notification.example.description}"
+        output.print "  " # Make the output line up
         super
         output.puts(notification.colorized_message_lines.join("\n"))
         output.puts(notification.colorized_formatted_backtrace.join("\n"))
