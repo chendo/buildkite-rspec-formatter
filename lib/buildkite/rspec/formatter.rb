9d58f1e9fd6a99cb5df265386e43a6062922185a
Jack Chen (chendo) <github@chen.do>
2016-12-01 14:28:16 +1100

    Expand output on failure if we have break on example set

--- lib/buildkite/rspec/formatter.rb
+++ lib/buildkite/rspec/formatter.rb
@@ -12,6 +12,7 @@ module Buildkite
       def initialize(output)
         super
         @max_depth = ENV.fetch('BUILDKITE_RSPEC_MAX_DEPTH', 1).to_i
+        @break_on_example = !!ENV['BUILDKITE_RSPEC_BREAK_ON_EXAMPLE']
       end
 
       def example_group_started(notification)
@@ -20,11 +21,11 @@ module Buildkite
       end
 
       def example_started(notification)
-        output.puts "--- #{prefix} #{notification.example.description}" if ENV['BUILDKITE_RSPEC_BREAK_ON_EXAMPLE']
+        output.puts "--- #{prefix} #{notification.example.description}" if @break_on_example
       end
 
       def example_failed(notification)
-        output.puts "+++ #{prefix} #{notification.example.description}" unless ENV['BUILDKITE_RSPEC_BREAK_ON_EXAMPLE']
+        output.puts "+++ #{prefix} #{notification.example.description}" unless @break_on_example
         output.print "   " # Make the output line up
         super
         output.puts(notification.colorized_message_lines.join("\n"))
@@ -33,6 +34,7 @@ module Buildkite
         if defined?(CapybaraInlineScreenshot) && screenshot = notification.example.metadata[:screenshot]
           output.puts CapybaraInlineScreenshot.escape_code_for_image(screenshot[:image]) if screenshot[:image]
         end
+        output.puts "+++ ^^^" if @break_on_example
         output.puts "--- –––"
       end
 
